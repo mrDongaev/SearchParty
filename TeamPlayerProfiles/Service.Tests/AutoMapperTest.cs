7c3ae65797ee56e696aa7ef6db3dd671bb3f1a3a
SattarovMK <sattarovmk@yandex.ru>
2024-09-30 00:46:33 +0500

    feat: test init

---
+++ TeamPlayerProfiles/Service.Tests/AutoMapperTest.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Service.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Tests
+{
+    public class AutoMapperTest
+    {
+        [Test]
+        public void AutoMapper_BetweenServiceAndDataAccess_IsCorrect()
+        {
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<HeroMappingProfile>();
+                cfg.AddProfile<PositionMappingProfile>();
+                cfg.AddProfile<PlayerMappingProfile>();
+                cfg.AddProfile<TeamMappingProfile>();
+            });
+
+            configuration.AssertConfigurationIsValid();
+        }
+    }
+}
