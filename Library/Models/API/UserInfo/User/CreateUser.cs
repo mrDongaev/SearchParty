7f47941ab8688edf4f192d8e271a908887def0ed
SattarovMK <sattarovmk@yandex.ru>
2024-11-05 20:46:59 +0500

    feat: userinfo library models, docker fix

---
+++ Library/Models/API/UserInfo/User/CreateUser.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models.User
+{
+    public static class CreateUser
+    {
+        public sealed class Request
+        {
+            public string? Name { get; set; }
+
+            public string? Description { get; set; }
+
+            public uint Mmr { get; set; }
+
+            public string? SteamFriendCode { get; set; }
+
+            public string? DiscordName { get; set; }
+
+            public string? TelegramLink { get; set; }
+        }
+    }
+}
