a84a4a27f2b7a101e7f7ecd077e608d03e03b486
Владимир <@DESKTOP-1F86S1L>
2024-11-08 00:13:47 +0300

    feat: web init

---
+++ SearchPartyFront/SearchPartyWeb/SearchPartyWeb/SearchPartyWeb/Core/ProfileRepository/ProfileService.cs
@@ -0,0 +1,8 @@
+﻿using SearchPartyWeb.Core.ApiExecutor;
+
+namespace SearchPartyWeb.Core.ProfileRepository;
+
+public class ProfileService(IWebApiExecutor _apiExecutor)
+{
+    async Task<> Get
+}
---
+++ SearchPartyFront/SearchPartyWeb/SearchPartyWeb/SearchPartyWeb/Program.cs
@@ -0,0 +1,34 @@
+
+using SearchPartyWeb.Components;
+using SearchPartyWeb.Core.ApiExecutor;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddTransient<IWebApiExecutor, WebApiExecutor>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode();
+
+app.Run();
