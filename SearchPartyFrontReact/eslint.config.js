328251c0fb3cf7d6b01abed3de11b1f09a9682b2
SattarovMK <sattarovmk@yandex.ru>
2025-03-22 04:04:22 +0500

    chore: linting and formatting config

--- SearchPartyFrontReact/eslint.config.js
+++ SearchPartyFrontReact/eslint.config.js
@@ -1,28 +1,54 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import reactX from "eslint-plugin-react-x";
+import reactDom from "eslint-plugin-react-dom";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+	{ ignores: ["dist"] },
+	{
+		extends: [
+			js.configs.recommended,
+			...tseslint.configs.strictTypeChecked,
+			...tseslint.configs.stylisticTypeChecked,
+		],
+		files: ["**/*.{ts,tsx}"],
+		languageOptions: {
+			ecmaVersion: "latest",
+			globals: globals.browser,
+			parserOptions: {
+				project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+		plugins: {
+			"react-hooks": reactHooks,
+			"react-refresh": reactRefresh,
+			"react-x": reactX,
+			"react-dom": reactDom,
+		},
+		rules: {
+			...reactHooks.configs.recommended.rules,
+			"react-refresh/only-export-components": [
+				"warn",
+				{
+					allowConstantExport: true,
+				},
+			],
+			...reactX.configs["recommended-typescript"].rules,
+			...reactDom.configs.recommended.rules,
+			"react-dom/no-missing-button-type": "off",
+			"react-dom/no-unsafe-target-blank": "off",
+		},
+	},
+	eslintPluginPrettierRecommended,
+	{
+		name: "custom",
+		rules: {
+			"prettier/prettier": ["warn"],
+		},
+	}
+);
---
+++ SearchPartyFrontReact/prettier.config.js
@@ -0,0 +1,17 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    printWidth: 100,
+    semi: true,
+    singleQuote: false,
+    jsxSingleQuote: false,
+    bracketSameLine: true,
+    arrowParens: "always",
+    useTabs: true,
+};
+
+export default config;
