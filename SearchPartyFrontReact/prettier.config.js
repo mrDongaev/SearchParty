441f090f000752f9281bbc84e2208354190b8e2f
SattarovMK <sattarovmk@yandex.ru>
2025-03-22 04:08:01 +0500

    chore: formatting

--- SearchPartyFrontReact/prettier.config.js
+++ SearchPartyFrontReact/prettier.config.js
@@ -3,15 +3,15 @@
  * @type {import("prettier").Config}
  */
 const config = {
-    trailingComma: "es5",
-    tabWidth: 4,
-    printWidth: 100,
-    semi: true,
-    singleQuote: false,
-    jsxSingleQuote: false,
-    bracketSameLine: true,
-    arrowParens: "always",
-    useTabs: true,
+	trailingComma: "es5",
+	tabWidth: 4,
+	printWidth: 100,
+	semi: true,
+	singleQuote: false,
+	jsxSingleQuote: false,
+	bracketSameLine: true,
+	arrowParens: "always",
+	useTabs: true,
 };
 
 export default config;
